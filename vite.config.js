5f85b981bc633326203bee77900f73af1e098659
nt4c1 <ankitsubedi543@gmail.com>
2025-07-20 21:12:32 +0545

    deploy

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/website/',  // 🔁 This must match your GitHub repo name exactly
   plugins: [react()],
+  base: '/website/',
 });
